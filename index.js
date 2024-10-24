63b6b440968b5db617fe2a8c37a628734a92a29e
shaharulsiyam <shaharulsiyam0273@gmail.com>
2024-10-24 12:20:31 +0600

    s

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(process.env.PORT);  
+    console.log(`Server is running on port 167.99.52.98:${PORT}`);
+});
