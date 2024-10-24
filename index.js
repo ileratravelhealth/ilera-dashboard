fb978e5eeb055e7c3d6b72b455c3d28555e6aaeb
shaharulsiyam <shaharulsiyam0273@gmail.com>
2024-10-24 12:21:00 +0600

    s

--- index.js
+++ index.js
@@ -16,6 +16,5 @@ app.get('*', (req, res) => {
 });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(process.env.PORT);  
     console.log(`Server is running on port 167.99.52.98:${PORT}`);
 });
