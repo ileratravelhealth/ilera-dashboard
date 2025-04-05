3853996e80c00ba9c884182186f16fac8e25725a
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2025-04-05 17:40:40 +0600

    fix issue

--- src/Utils/BaseUrl.js
+++ src/Utils/BaseUrl.js
@@ -1,5 +1,8 @@
 export const isDev = import.meta.env.NODE_ENV !== 'production'
-const devUrl = import.meta.env.VITE_LOCAL_SERVER_URL ?? 'http://13.43.16.29:5000'
-const prodUrl = import.meta.env.VITE_SERVER_PROD_URL ?? 'http://13.43.16.29:5000'
+const devUrl = "http://10.0.60.189:5000"
+const prodUrl = "http://10.0.60.189:5000"
+// export const isDev = import.meta.env.NODE_ENV !== 'production'
+// const devUrl = import.meta.env.VITE_LOCAL_SERVER_URL ?? 'http://13.43.16.29:5000'
+// const prodUrl = import.meta.env.VITE_SERVER_PROD_URL ?? 'http://13.43.16.29:5000'
 
 export const url = isDev ? devUrl : prodUrl
