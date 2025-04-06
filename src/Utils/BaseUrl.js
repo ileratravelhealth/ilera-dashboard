2749c4bd37b296210e31033f0495d8f0db194cad
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2025-04-06 13:23:47 +0600

    urls

--- src/Utils/BaseUrl.js
+++ src/Utils/BaseUrl.js
@@ -1,4 +1,4 @@
-export const isDev = import.meta.env.NODE_ENV !== 'production'
+export const isDev = import.meta.env.NODE_ENV !== 'production'//
 const devUrl = import.meta.env.VITE_LOCAL_SERVER_URL ?? 'http://13.43.16.29:5000'
 const prodUrl = import.meta.env.VITE_SERVER_PROD_URL ?? 'http://13.43.16.29:5000'
 
